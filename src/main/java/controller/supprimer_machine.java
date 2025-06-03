b1655a87c20e44cadca5922d059511c039616949
cmoinecorneill01 <cmoinecorneill01@PC-0283.csip.in>
2025-06-03 14:46:41 +0200

    Modif Célia

---
+++ src/main/java/controller/supprimer_machine.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import util.FichierMachine;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author cmoinecorneill01
+ */
+public class supprimer_machine {
+    
+}
