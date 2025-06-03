201bf64ca20835306004314c6c5d8258d3ba0beb
louis <louis@172.29.105.92>
2025-06-03 17:34:50 +0200

    Merge origin/master
    # Conflicts:
    #	src/main/java/module-info.java

--- src/main/java/controller/supprimer_machine.java
+++ src/main/java/controller/supprimer_machine.java
@@ -6,7 +6,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import model.Machine;
 import util.FichierMachine;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class supprimer_machine {
 
@@ -43,16 +47,27 @@ public class supprimer_machine {
                 messageLabel.setText("Veuillez entrer une référence.");
                 return;
             }
-
-            boolean success = FichierMachine.supprimerMachine(refTexte);
-            if (success) {
-                messageLabel.setStyle("-fx-text-fill: green;");
-                messageLabel.setText("Machine supprimée avec succès.");
-                nomField.clear();
-            } else {
+            List<Machine> tempList = new ArrayList<>(FichierMachine.getListeMachines());
+            boolean found = false;
+            for (Machine m : tempList) {
+                if (m.getRefMachine().equalsIgnoreCase(refTexte)) {
+                    tempList.remove(m);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
                 messageLabel.setStyle("-fx-text-fill: red;");
                 messageLabel.setText("Machine non trouvée.");
+                return;
             }
+            FichierMachine.clearListeMachines();
+            for (Machine m : tempList) {
+                FichierMachine.ajouterMachine(m);
+            }
+            messageLabel.setStyle("-fx-text-fill: green;");
+            messageLabel.setText("Machine supprimée avec succès.");
+            nomField.clear();
         });
 
         Scene scene = new Scene(grid, 400, 200);
